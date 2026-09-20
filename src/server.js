4457c51da1c8f5ed47b8bca338afa1aec17ac6e3
Larysa Ivshyna <ivshynalarysa@gmail.com>
2026-09-20 20:19:44 +0100

    correct connection db

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import cors from  'cors';
 import express from 'express';
 import 'dotenv/config';
 
-import { connectMongoDB } from 'db/connectMongoDB.js';
+import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 
 import { notFoundHandler } from './middleware/notFoundHandler.js';
