55786c118d235477ba9bffd8da939df05812109a
Larysa Ivshyna <ivshynalarysa@gmail.com>
2026-09-20 20:00:29 +0100

    add put delete patch

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -2,7 +2,7 @@ import { createHttpError } from 'http-errors';
 import { Note } from '../models/note.js';
 
 // Отримати список усіх нотаток
-export const getNotes = async (req, res) => {
+export const getAllNotes = async (req, res) => {
   const notes = await Note.find();
   res.status(200).json({ notes });
 };
@@ -17,5 +17,39 @@ export const getNoteById = async (req, res) => {
   }
 
   res.status(200).json( note);
+};
+
+//create new note
+export const createNote = async (req, res) => {
+  const note = await Note.create(req.body);
+  res.status(201).json({ note });
+};
+
+// delete note by Id
+export const deleteNote = async (req, res) => {
+  const { noteId } = req.params;
+  const note = await Note.findOneAndDelete({ _id: noteId });
+
+  if (!note) {
+    throw new createHttpError(404, 'Note not found');
+  }
+
+  res.status(200).json( note, { message: 'Note deleted successfully' });
+};
+
+//update note by Id
+export const updateNote = async (req, res) => {
+  const { noteId } = req.params;
+  const note = await Note.findOneAndUpdate(
+    { _id: noteId }, // find by id
+    req.body,
+    { returnDocument: "after" }
+  );
+
+  if (!note) {
+    throw createHttpError(404, 'Note not found');
+  }
+
+  res.status(200).json({ note });
 };
 
--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -1,13 +1,21 @@
 
 
 import { Router } from 'express';
-import { getNotes, getNoteById } from '../controllers/notesController.js';
+import {
+  getAllNotes,
+  getNoteById,
+  createNote,
+  deleteNote,
+  updateNote
+} from '../controllers/notesController.js';
 
 const router = Router();
 
-router.get('/notes', getNotes);
+router.get('/notes', getAllNotes);
 router.get('/notes/:noteId', getNoteById);
-
+router.post('/notes', createNote);
+router.delete('/notes/:noteId', deleteNote);
+router.put('/notes/:noteId', updateNote);
 
 
 export default router;
