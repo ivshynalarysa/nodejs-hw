225127ca299db9e82958c29d8573b2d8989d253c
Larysa Ivshyna <ivshynalarysa@gmail.com>
2026-09-23 22:27:01 +0100

    correct import TAGS

--- src/models/note.js
+++ src/models/note.js
@@ -1,5 +1,5 @@
 import { Schema, model } from "mongoose";
-import TAGS from '../constants/tags.js';
+import { TAGS } from '../constants/tags.js';
 
 const noteSchema = new Schema(
   {
--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -1,6 +1,6 @@
 import { Joi, Segments } from 'celebrate';
 import { isValidObjectId } from 'mongoose';
-import TAGS from '../constants/tags.js';
+import { TAGS } from '../constants/tags.js';
 
 // Кастомний валідатор для ObjectId
 const objectIdValidator = (value, helpers) => {
